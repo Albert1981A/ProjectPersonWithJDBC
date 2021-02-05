a2ada5a598815c84f5c8db6959a6682340a5583a
Albert Abuav <albrtabo@gmail.com>
2021-02-05 21:07:57 +0200

    Changing The Date method NUM 2

--- src/com/AlbertAbuav/PlayTestDate.java
+++ src/com/AlbertAbuav/PlayTestDate.java
@@ -1,5 +1,7 @@
 package com.AlbertAbuav;
 
+import com.AlbertAbuav.beans.Hobby;
+import com.AlbertAbuav.beans.Person;
 import com.AlbertAbuav.utils.DateUtils;
 
 import java.time.LocalDate;
@@ -10,15 +12,18 @@ public class PlayTestDate {
     public static void main(String[] args) {
 
         // Hardcoded date
-        Date date1 = DateUtils.getDate(23, 3, 2002, 12, 20, 23);
-        System.out.println(date1);
+        Date date1 = DateUtils.getDate(25, 3, 2003, 22, 2, 23);
+        System.out.println(DateUtils.beautifyDate(date1));
+
+        Person person = new Person("avi", "TLV", DateUtils.getDate(25, 3, 2003, 22, 2, 23), Hobby.GO_TO_THE_BEACH);
+        System.out.println(person);
 
         // getting the local date and it will be reset every time the program will turn on
         Date date2 = DateUtils.javaDateFromLocalDate(LocalDate.now());
-        System.out.println(date2);
+        System.out.println(DateUtils.beautifyDate(date2));
 
         // getting the local date plus a week and it will be reset every time the program will turn on
         Date date3 = DateUtils.javaDateFromLocalDate(LocalDate.now().plusWeeks(1));
-        System.out.println(date3);
+        System.out.println(DateUtils.beautifyDate(date3));
     }
 }
